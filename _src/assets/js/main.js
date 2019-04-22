166623b784aed95d4815c90acc440b4f413f3361
antonellam <antonella.michelli@gmail.com>
2019-04-22 19:08:43 +0200

    Favorite list feature

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,23 +3,48 @@
 const searchButtonEl = document.querySelector('.search__button');
 const searchInputEl = document.querySelector('.search__input');
 const searchResponseEl = document.querySelector('.results-section');
+const favoriteSectionEl = document.querySelector('.favorite-section');
+
+window.localStorage.clear();
 
 let search = '';
 let favorites = [];
 
 function cardClickFavoriteHandler(event) {
-  console.log('Listener works', event.currentTarget);
-  const selectedCard = event.currentTarget;
-  selectedCard.classList.toggle('results-section__card--selected');
-  if (selectedCard.classList.contains('results-section__card--selected')) {
-    favorites.push(selectedCard.innerHTML);
+  if (event.currentTarget.classList.contains('results-section__card--selected') === false) {
+    event.currentTarget.classList.add('results-section__card--selected');
+    const favoriteListItem = document.createElement('li');
+    const favoriteTitleEl = document.createElement('h2');
+    const favoriteTitle = document.createTextNode(event.currentTarget.firstChild.innerHTML);
+    favoriteTitleEl.appendChild(favoriteTitle);
+    const favoriteImg = document.createElement('img');
+    favoriteImg.setAttribute('style', `background-image: ${event.currentTarget.firstChild.nextElementSibling.style.backgroundImage}`);
+    favoriteImg.classList.add('results-section__image');
+    favoriteListItem.appendChild(favoriteTitleEl);
+    favoriteListItem.appendChild(favoriteImg);
+    favoriteListItem.setAttribute('id', event.currentTarget.firstChild.id);
+    favoriteSectionEl.appendChild(favoriteListItem);
+
+    const myFavoriteSerie = {
+      id: event.currentTarget.firstChild.id,
+      name: event.currentTarget.firstChild.innerHTML,
+      img: event.currentTarget.firstChild.nextElementSibling.style.backgroundImage,
+    };
+
+    console.log(myFavoriteSerie);
+    favorites.push(myFavoriteSerie);
     localStorage.setItem('favorites', JSON.stringify(favorites));
 
-  } else {
-    const cardPosition = favorites.indexOf(selectedCard.innerHTML);
-    favorites.splice(cardPosition, cardPosition, '');
-    localStorage.setItem('favorites', JSON.stringify(favorites));
+  } else if (event.currentTarget.classList.contains('results-section__card--selected') === true) {
+    event.currentTarget.classList.remove('results-section__card--selected');
+    const removedId = document.getElementById(`${event.currentTarget.firstChild.id}`);
+    removedId.remove();
 
+
+
+    const cardPosition = favorites.indexOf(event.currentTarget.firstChild.id);
+    favorites.splice(cardPosition-1);
+    localStorage.setItem('favorites', JSON.stringify(favorites));
   }
 }
 
@@ -28,6 +53,7 @@ function printSeriesTitle(seriesObject) {
   const titleEl = document.createElement('h2');
   const title = document.createTextNode(seriesObject.name);
   titleEl.appendChild(title);
+  titleEl.setAttribute('id', `${seriesObject.id}`);
   seriesCard.appendChild(titleEl);
   searchResponseEl.appendChild(seriesCard);
   printSeriesImage(seriesCard, seriesObject);
@@ -56,6 +82,8 @@ function searchSeries(title) {
   fetch(`http://api.tvmaze.com/search/shows?q=${title}`)
     .then(response => response.json())
     .then(function (data) {
+      searchResponseEl.innerHTML = '';
+
       for (const serie of data) {
         printSeriesTitle(serie.show);
       }
